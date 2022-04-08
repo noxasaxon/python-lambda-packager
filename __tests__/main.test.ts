02d6b604bfe54dc7e281d99b461448e8f1a7611b
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2022-04-07 23:56:49 -0700

    Update main.test.ts

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import { DEFAULT_FUNCTIONS_DIR_NAME } from '../lib/constants';
 import { makePackages, defaultPackagingArgs } from '../lib/index';
 
 const MOCK_REPO_PATH = path.join(__dirname, './mock_repo');
@@ -6,7 +7,10 @@ const MOCK_REPO_PATH = path.join(__dirname, './mock_repo');
 describe('makePackages', () => {
   it('should be run', async () => {
     const pkgArgs = defaultPackagingArgs;
-    pkgArgs.functionsDir = path.join(MOCK_REPO_PATH, 'functions');
+    pkgArgs.functionsDir = path.join(
+      MOCK_REPO_PATH,
+      DEFAULT_FUNCTIONS_DIR_NAME,
+    );
 
     // run function
     await makePackages(pkgArgs);
